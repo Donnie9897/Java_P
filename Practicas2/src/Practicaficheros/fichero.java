121e2cb97099f5aa54914aeaf9321db94d3e28d2
Donnie9897 <49081151+Donnie9897@users.noreply.github.com>
2020-12-27 10:16:29 -0400

    Add files via upload

---
+++ Practicas2/src/Practicaficheros/fichero.java
@@ -0,0 +1,5 @@
+package Practicaficheros;
+
+public class fichero {
+
+}
---
+++ Practicas2/src/Practicaficheros/main.java
@@ -0,0 +1,15 @@
+package Practicaficheros;
+
+import java.io.File;
+import java.io.FileWriter;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		File archivo = new File();
+		FileWriter escritor;
+		
+	}
+
+}
